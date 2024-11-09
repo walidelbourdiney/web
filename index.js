cec72ad6eb6031dec802f47b5d354bbf4550d083
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-11-09 14:45:01 +0200

    higher class functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+console.log(upperFirstWord("hello there how are you"));
+
+const greet = (greeting) => {
+  return (name) => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greet("hi")("name");
