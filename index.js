1a015b7bbc649073e65e99d0e85c16a8e96e5fed
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-11-14 18:01:17 +0200

    construction functions and classes

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-"use strict";
+// const car = function (make, speed) {
+//   (this.make = make), (this.speed = speed);
+// };
+
+// const car1 = new car("honda", 100);
+// console.log(car1);
+
+// car.prototype.accelerate = function () {
+//   const newSpeed = this.speed + 10;
+//   console.log(newSpeed);
+// };
+
+// car1.accelerate();
+class Person {
+  constructor(fName, birthYear) {
+    (this.fName = fName), (this.birthYear = birthYear);
+  }
+  calcAge() {
+    console.log(2030 - this.birthYear);
+  }
+  showName() {
+    console.log(this.fName);
+  }
+}
+
+const Ali = new Person("Ali", 1990);
+
+console.log(Ali.__proto__ === Person.prototype);
