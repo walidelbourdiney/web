9062117bf4ebd564ed4ae71e60703b3388d3b330
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-11-14 19:06:53 +0200

    Asynchronous JS

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// const car = function (make, speed) {
-//   (this.make = make), (this.speed = speed);
-// };
+"use strict";
 
-// const car1 = new car("honda", 100);
-// console.log(car1);
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
-// car.prototype.accelerate = function () {
-//   const newSpeed = this.speed + 10;
-//   console.log(newSpeed);
-// };
-
-// car1.accelerate();
-class Person {
-  constructor(fName, birthYear) {
-    (this.fName = fName), (this.birthYear = birthYear);
-  }
-  calcAge() {
-    console.log(2030 - this.birthYear);
-  }
-  showName() {
-    console.log(this.fName);
-  }
-}
-
-const Ali = new Person("Ali", 1990);
-
-console.log(Ali.__proto__ === Person.prototype);
+///////////////////////////////////////
+alert("hi");
