66993dfa0e98149e005fedf6c584beacdf61c6d7
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-11-15 19:28:07 +0200

    GET api DATA

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-alert("hi");
+//
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all");
+request.send();
+
+request.addEventListener("load", function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data[0]);
+});
