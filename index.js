ab45e53944e274d4f0b9bac2fa7e6bc6e3752d8d
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-11-16 11:42:25 +0200

    Modern API request

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 //
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all");
-request.send();
+// const request = new XMLHttpRequest();
+// request.open("GET", "https://restcountries.com/v3.1/all");
+// request.send();
 
-request.addEventListener("load", function () {
-  const data = JSON.parse(this.responseText);
-  console.log(data[0]);
-});
+// request.addEventListener("load", function () {
+//   const data = JSON.parse(this.responseText);
+//   console.log(data[0]);
+// });
+
+const data = fetch("https://countries-api-836d.onrender.com/countries");
