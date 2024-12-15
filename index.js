6e127a48cbc2f67aabaa271a42be465543971114
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-12-15 20:14:21 +0200

    pokemon game

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 "use strict";
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-  .then((response) => {
+const form = document.querySelector("form"); // The form element
+
+const submitButton = document.querySelector(".submit"); // The button element
+const image = document.getElementById("pokemonimg");
+const inputName = document.getElementById("name"); // The input element
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevents the page from refreshing on form submission
+  const pokemonName = inputName.value;
+  getPokemon(pokemonName);
+});
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("could not fetch Data");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data.weight))
+//   .catch((error) => console.error(error));
+
+async function getPokemon(name) {
+  try {
+    // Example usage
+    // Gets the value from the input field
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     if (!response.ok) {
-      throw new Error("could not fetch Data");
+      throw new Error("could not find resource");
     }
-    return response.json();
-  })
-  .then((data) => console.log(data.weight))
-  .catch((error) => console.error(error));
+    const data = await response.json();
+
+    image.src = data.sprites.front_default;
+    image.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
