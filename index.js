4a62423a0d44ed92cc15465da7ddc092fe4fffa1
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-12-15 19:32:46 +0200

    fetch/then method

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
-
-///////////////////////////////////////
-//
-// const request = new XMLHttpRequest();
-// request.open("GET", "https://restcountries.com/v3.1/all");
-// request.send();
-
-// request.addEventListener("load", function () {
-//   const data = JSON.parse(this.responseText);
-//   console.log(data[0]);
-// });
-
-const data = fetch("https://countries-api-836d.onrender.com/countries");
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("could not fetch Data");
+    }
+    return response.json();
+  })
+  .then((data) => console.log(data.weight))
+  .catch((error) => console.error(error));
