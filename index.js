f4855a9cf3b13b0cd096d43333dae7a9965c45af
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-12-16 18:53:00 +0200

    add guessing game

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const form = document.querySelector("form"); // The form element
+// const form = document.querySelector("form"); // The form element
 
-const submitButton = document.querySelector(".submit"); // The button element
-const image = document.getElementById("pokemonimg");
-const inputName = document.getElementById("name"); // The input element
+// const submitButton = document.querySelector(".submit"); // The button element
+// const image = document.getElementById("pokemonimg");
+// const inputName = document.getElementById("name"); // The input element
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); // Prevents the page from refreshing on form submission
-  const pokemonName = inputName.value.toLowerCase();
-  getPokemon(pokemonName);
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault(); // Prevents the page from refreshing on form submission
+//   const pokemonName = inputName.value.toLowerCase();
+//   getPokemon(pokemonName);
+// });
 
 // fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
 //   .then((response) => {
@@ -22,29 +22,57 @@ form.addEventListener("submit", (e) => {
 //   .then((data) => console.log(data.weight))
 //   .catch((error) => console.error(error));
 
-async function getPokemon(name) {
-  try {
-    // Example usage
-    // Gets the value from the input field
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    if (!response.ok) {
-      throw new Error("could not find resource");
-    }
-    const data = await response.json();
+// async function getPokemon(name) {
+//   try {
+//     // Example usage
+//     // Gets the value from the input field
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+//     if (!response.ok) {
+//       throw new Error("could not find resource");
+//     }
+//     const data = await response.json();
 
-    image.src = data.sprites.front_default;
-    image.style.display = "block";
-  } catch (error) {
-    console.error(error);
+//     image.src = data.sprites.front_default;
+//     image.style.display = "block";
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// let password = "";
+
+// for (let i = 0; i < 8; i++) {
+//   let randomIndex = Math.floor(Math.random() * chars.length);
+//   password += chars[randomIndex];
+// }
+
+// console.log(password);
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + 1);
+console.log(answer);
+let attempts = 0;
+let running = true;
+
+while (running) {
+  let guess = window.prompt("Guess a number between 1 and 100.");
+  guess = Number(guess);
+  if (isNaN(guess)) {
+    alert("Please insert a valid number!");
+  } else if (guess < minNum || guess > maxNum) {
+    alert("Please insert a valid number!");
+  } else {
+    if (guess > answer) {
+      attempts++;
+      alert("The number you have inserted is too high!");
+    } else if (guess < answer) {
+      attempts++;
+      alert("The number you have inserted is too low!");
+    } else {
+      alert(`Congratulations! you have done it after ${attempts} attempts.`);
+      running = false;
+    }
   }
 }
-
-let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-let password = "";
-
-for (let i = 0; i < 8; i++) {
-  let randomIndex = Math.floor(Math.random() * chars.length);
-  password += chars[randomIndex];
-}
-
-console.log(password);
