7e7cd244894a54b7fec74f0d8857b9e6236403bc
walidelbourdiney <walidelbourdiney25@gmail.com>
2024-12-19 00:23:05 +0200

    editing

--- index.js
+++ index.js
@@ -49,30 +49,31 @@
 
 // console.log(password);
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + 1);
-console.log(answer);
-let attempts = 0;
-let running = true;
+// Guessing Game
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + 1);
+// console.log(answer);
+// let attempts = 0;
+// let running = true;
 
-while (running) {
-  let guess = window.prompt("Guess a number between 1 and 100.");
-  guess = Number(guess);
-  if (isNaN(guess)) {
-    alert("Please insert a valid number!");
-  } else if (guess < minNum || guess > maxNum) {
-    alert("Please insert a valid number!");
-  } else {
-    if (guess > answer) {
-      attempts++;
-      alert("The number you have inserted is too high!");
-    } else if (guess < answer) {
-      attempts++;
-      alert("The number you have inserted is too low!");
-    } else {
-      alert(`Congratulations! you have done it after ${attempts} attempts.`);
-      running = false;
-    }
-  }
-}
+// while (running) {
+//   let guess = window.prompt("Guess a number between 1 and 100.");
+//   guess = Number(guess);
+//   if (isNaN(guess)) {
+//     alert("Please insert a valid number!");
+//   } else if (guess < minNum || guess > maxNum) {
+//     alert("Please insert a valid number!");
+//   } else {
+//     if (guess > answer) {
+//       attempts++;
+//       alert("The number you have inserted is too high!");
+//     } else if (guess < answer) {
+//       attempts++;
+//       alert("The number you have inserted is too low!");
+//     } else {
+//       alert(`Congratulations! you have done it after ${attempts} attempts.`);
+//       running = false;
+//     }
+//   }
+// }
